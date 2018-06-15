4567482c7916fd33565083387a5fd4372faed910
Daniel Ajilore <mtme13@hotmail.com>
2018-06-15 17:11:25 +0100

    -w

--- weather/weather.js
+++ weather/weather.js
@@ -1,5 +1,4 @@
 const request = require('request');
-<<<<<<< HEAD
 
 let getWeather = (lat, lng) => {
 
@@ -18,23 +17,6 @@ let getWeather = (lat, lng) => {
             }
         });
     });    
-=======
-let weatherKey;
-let getWeather = (lat, lng, callback) => {
-    request({
-        url: `https://api.darksky.net/forecast/${weatherKey}/${lat},${lng}?units=uk2`,
-        json: true
-    }, (error, response, body) => {
-        if (!error && response.statusCode === 200) {            
-            callback(undefined, {
-                temperature: body.currently.temperature,
-                apparentTemperature: body.currently.apparentTemperature
-            });
-        }else {            
-            callback('Unable to fetch weather');
-        }
-    });
->>>>>>> 92a83ccd04931e10113884fd7ff7c6650d3c4622
 };
 
 
